0bdc37c74abbc4ee6e53dd6f9f1c100d30b82fc0
Paavo Manninen <uunis95@gmail.com>
2017-09-06 10:57:32 +0300

    Added blackjack stage1 and viikkotehtava 1 template.

--- Hello/BlackJack.cs
+++ Hello/BlackJack.cs
@@ -11,25 +11,35 @@ namespace Hello
         {
             int myNumber = 17;
             int theirNumber;
-            System.Console.WriteLine("*** BlackJack! ***");
-            System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-            //reading and converting 
-            theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-            //comparing that given umber is valid
-            if (theirNumber < 1 || theirNumber > 21)
+
+
+
             {
-                Console.WriteLine("The given number is out of limits, try again.");
-            }
-            else
-            {
-                //comparing
-                if (theirNumber >= myNumber && theirNumber <= 21)
+                System.Console.WriteLine("*** BlackJack! ***");
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                string consent = Console.ReadLine();
+                theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                //comparing that given umber is valid
+                while (consent != "x")
                 {
-                    System.Console.WriteLine("You win.");
-                }
-                else
-                {
-                    System.Console.WriteLine("You lose.");
+                    do
+                    {
+                        Console.WriteLine("The given number is out of limits, try again.");
+                        System.Console.Write("Enter any number between 1-21: ");
+                        theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+
+                    } while (theirNumber < 1 || theirNumber > 21);
+
+                    //comparing
+                    if (theirNumber >= myNumber && theirNumber <= 21)
+                    {
+                        System.Console.WriteLine("You win.");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("You lose.");
+                    }
                 }
             }
         }
