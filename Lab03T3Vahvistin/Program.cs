96341e7f131c8c1874134a1faec991e31775990e
Paavo Manninen <uunis95@gmail.com>
2017-09-25 18:57:42 +0300

    Added Lab03 T3

---
+++ Lab03T3Vahvistin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03T3Vahvistin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetVolume();
+        }
+        static void GetVolume()
+        {
+            Vahvistin uusivahvistin = new Vahvistin();
+
+            while (true)
+            {
+                Console.WriteLine("Anna voluumi 0-100 >");
+                uusivahvistin.Volume = int.Parse(Console.ReadLine());
+                Console.WriteLine("Volume:> " + uusivahvistin.Volume);
+            }
+        }
+
+
+    }
+}
