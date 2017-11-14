670610e6ee822be575c715f7f9af57181ce87419
Paavo Manninen <uunis95@gmail.com>
2017-11-14 22:36:39 +0200

    Added Lab10T1,T2 & T3

--- Lab10T02/MainWindow.xaml.cs
+++ Lab10T02/MainWindow.xaml.cs
@@ -20,14 +20,68 @@ namespace Lab10T02
     /// </summary>
     public partial class MainWindow : Window
     {
+        double number;
+        double number2;
+
+        int integeeri;
+        string stringuliini;
+        string usd = "Yhdysvallat USD";
+        string eur = "Suomi EUR";
+        string kirjain = "Syötä numeroita";
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        private void dropBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+                ListBoxItem listitem = (ListBoxItem)dropBox1.SelectedItem;
+                if ((ListBoxItem)dropBox1.SelectedItem == usd1)
+                {
+                    txtBlock3.Text = eur.ToString();
+                }
+                else if ((ListBoxItem)dropBox1.SelectedItem == eur1)
+                {
+                    txtBlock3.Text = usd.ToString();
+                }
+                else
+                {
+                    txtBlock3.Text = "Kohde valuutta";
+                }
+                
+            
+        }
         private void txtBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+            try
+            {
+                    if (int.TryParse(txtBox1.Text, out integeeri))
+                    {
+                    txtBlock1.Text = null;
+                        if ((ListBoxItem)dropBox1.SelectedItem == usd1)
+                        {
+                            number = double.Parse(txtBox1.Text);
+                            number2 = number * 0.8498; //googlesta
+                            txtBox2.Text = number2.ToString("0.00");
+                        }
+                        else if ((ListBoxItem)dropBox1.SelectedItem == eur1)
+                        {
+                            number = double.Parse(txtBox1.Text);
+                            number2 = number * 1.1766; //googlesta
+                            txtBox2.Text = number2.ToString("0.00");
+                        }
+                    }
+                    else
+                    {
+                        txtBlock1.Text = kirjain.ToString();
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+                txtBlock4.Text = ex.Message;   
+            }
+
         }
     }
 }
---
+++ Lab10T03/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab10T03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        int leveys;
+        int korkeus;
+        int karmipuunleveys;
+
+        int Aikkuna;
+        int Alasi;
+        int Piiri;
+        int integeer;
+
+        string virhe = "Syöte ei ole numero";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtBox1.Text, out integeer))
+            {
+                txtBlock5.Text = null;
+                leveys = int.Parse(txtBox1.Text);
+                if (int.TryParse(txtBox2.Text, out integeer))
+                {
+                    txtBlock6.Text = null;
+                    korkeus = int.Parse(txtBox2.Text);
+                    if (int.TryParse(txtBox3.Text, out integeer))
+                    {
+                        txtBlock7.Text = null;
+                        karmipuunleveys = int.Parse(txtBox3.Text);
+                        Aikkuna = (leveys * korkeus) / 10;
+                        Alasi = (leveys - karmipuunleveys) * (korkeus - karmipuunleveys) / 10;
+                        Piiri = (leveys + leveys + korkeus + korkeus) / 10;
+
+                        txtBlock1.Text = Aikkuna.ToString() + "cm^2";
+                        txtBlock2.Text = Alasi.ToString() + "cm^2";
+                        txtBlock3.Text = Piiri.ToString() + "cm";
+
+                        ikkuna.Height = korkeus / 10;
+                        ikkuna.Width = leveys /10;
+                        ikkuna.StrokeThickness = karmipuunleveys / 10;
+                    }
+                    else
+                    {
+                        txtBlock7.Text = virhe.ToString();
+                    }
+                }
+                else
+                {
+                    txtBlock6.Text = virhe.ToString();
+                }
+            }
+            else
+            {
+                txtBlock5.Text = virhe.ToString();
+            }
+
+        }
+    }
+}
