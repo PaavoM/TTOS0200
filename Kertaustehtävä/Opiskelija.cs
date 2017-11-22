0d1f091adf05a1b76364b38bf0ae7f7a118332f1
Paavo Manninen <uunis95@gmail.com>
2017-11-22 11:12:16 +0200

    Added kertaustehtävä

---
+++ Kertaustehtävä/Opiskelija.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertaustehtävä
+{
+    public class Opiskelija
+    {
+        string Nimi { get; set; }
+        int Ikä { get; set; }
+        public static int opiskelijoidenlkm = 0;
+        public Opiskelija(string nimi, int ikä)
+        {
+            Nimi = nimi;
+            Ikä = ikä;
+            opiskelijoidenlkm++;
+        }
+        public static int Montako()
+        {
+            return opiskelijoidenlkm;
+        }
+
+        public override string ToString()
+        {
+            return "Oppilaan nimi: " + Nimi + " ja ikä " + Ikä;
+        }
+    }
+}
